105db160f8de0a73eaea870161124f301035e1a0
unikorm <adaled00@gmail.com>
2023-09-21 18:49:33 +0200

    i have second breath, i finnally understand basic logic of structuring next.js app, honestly, gpt help me with that, i think on basic question she not response so good as on specific one like this, she very nicely summarize this topic for me, help me create structure i needed and finnally i can move on to build something, jeyyyyy..

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,10 +1,15 @@
 
 import Link from "next/link";
+import Head from "next/head";
+
 import Layout from "../components/layout"
 
 const AboutSection = () => {
     return (
         <Layout>
+            <Head>
+                <title>about</title>
+            </Head>
             <p>about</p>
             <Link href="/">main back</Link>
         </Layout>
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,16 @@
 
-// import Image from 'next/image';
-
+import Image from 'next/image';
 import Link from "next/link";
+import Head from "next/head";
+
 import Layout from "../components/layout";
 
 const Home = () => {
   return (
     <Layout>
+      <Head>
+        <title>home</title>
+      </Head>
       <p>home</p>
       <Link href="/about-me">na mna</Link>
     </Layout>
