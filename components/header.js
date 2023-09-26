c4142b1a8add62dec4d112f2de61a9a9192d00cb
unikorm <adaled00@gmail.com>
2023-09-26 22:40:28 +0200

    workin on main page in index.js, a little modify on root layout and keep goin on footer/header and so on...

--- components/header.js
+++ components/header.js
@@ -1,5 +1,6 @@
 
 import Link from 'next/link';
+import styles from "../styles/layout.module.css";
 
 const Header = () => {
   return (
--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ import Head from "next/head";
 
 import Layout from "../components/layout";
 import PostList from '../components/postList';
+import styles from "../styles/layout.module.css";
 
 const Home = () => {
   return (
@@ -12,7 +13,17 @@ const Home = () => {
       <Head>
         <title>home</title>
       </Head>
-      <p>home</p>
+      <section className={styles.aboutMeOnMain}>
+        <Image
+          src="/images/profile-photo.jpg"
+          className={styles.profilePhotoMain}
+          height={190}
+          width={190}
+          alt="janula naša"
+        />
+        <h1>Janka LEDNICKÁ</h1>
+        <p>Som obyčajne dedinské dievča ktoré ľúbi dobrodružstvo, adrenalín, pohyb. Tu si môžte prečítať niečo s mojej reality ktorá nieje vždy ružová no ako sa vraví “čo ťa nezabije to ťa posilní “.🐧</p>
+      </section>
       <PostList />
       <Link href="/about-me">na mna</Link>
     </Layout>
@@ -20,3 +31,4 @@ const Home = () => {
 }
 
 export default Home;
+
