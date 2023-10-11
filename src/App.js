797c859ebc6bf4e38128433bb99e5e895c3caa0c
unikorm <adaled00@gmail.com>
2023-10-11 23:30:44 +0200

    Head component is done for now also with styling, now Blog

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 
 import './App.css';
 import Header from "../src/components/Header";
+import Head from './components/Head';
 import Footer from './components/Footer';
 
 import React from 'react';
+
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Head />
       <Footer />
     </React.Fragment>
   );
---
+++ src/components/Head.js
@@ -0,0 +1,17 @@
+
+import styles from "../styles/head.module.css";
+
+const Head = () => {
+
+    const profilePhoto = "/images/profile.webp";
+
+    return (
+        <section className={styles.headSection}>
+            <img src={process.env.PUBLIC_URL + profilePhoto} />
+            <h2>JANA LEDNICKÁ</h2>
+            <p>Som obyčajne dedinské dievča ktoré ľúbi dobrodružstvo, adrenalín, pohyb. Tu si môžte prečítať niečo s mojej reality ktorá nieje vždy ružová no ako sa vraví “čo ťa nezabije to ťa posilní “. 🐧</p>
+        </section>
+    );
+};
+
+export default Head;
