3dc62e9f7f3314da2e3bc3231db3948688992cc8
unikorm <adaled00@gmail.com>
2023-10-17 11:48:17 +0200

    great, i have modify url adress when i'm an article, make it simpler using array destructing on data of articles and i understand it a littel better

--- src/components/BlogList.js
+++ src/components/BlogList.js
@@ -1,24 +1,26 @@
 
 import styles from "../styles/blogList.module.css";
-import articles from "../data.json";
+import data from "../data.json";
 
 import React from "react";
 import { useNavigate } from 'react-router-dom'; 
 
 const BlogList = () => {
     const navigate = useNavigate();
+    const { articles } = data;
 
-    const onArticleClick = (articleId) => {
-        navigate(`/article/${articleId}`);
+    const onArticleClick = (articleTitle) => {
+        const url = articleTitle.split(" ").join("-");
+        navigate(`/${url}`);
     };
 
     return (
         <section className={styles.blogListSection}>
             <h4>MôJ BLOG</h4>
-            {articles.articles.map((article) => (
+            {articles.map((article) => (
                 <article
                     key={article.id}
-                    onClick={() => onArticleClick(article.id)}
+                    onClick={() => onArticleClick(article.title)}
                 >
                     <h5>{article.title}</h5>
                     <p>{article.content.slice(0, 100) + "..."}</p>
